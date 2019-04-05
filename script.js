b060d0ff12efb9ab98733eb46ec7d91c09b923a9
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-04-05 00:43:43 +0000

    moved API keys to untracked .gitignore file for security

---
+++ script.js
@@ -0,0 +1,14 @@
+function search() {
+    const keyword = document.getElementById("keyword").value;
+    console.log(keyword);
+    
+    // get results from API using fetch
+    https://www.goodreads.com/search/index.xml?key=syJG0KciOSzBmzFcpRYw&q=Ender%27s+Game
+    const endpoint = "https://www.goodreads.com/search/index.xml"
+    const apiKey = ""
+    
+    fetch(url)
+    .then(function(response) {
+        
+    })
+}
