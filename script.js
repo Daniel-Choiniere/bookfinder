406a0a104b5b6f67013ea3a3f674e38374721191
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-04-05 01:14:20 +0000

    built up API url with user key, CORS proxy and keyword search parameter

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+/*global goodreadsApiKey */
+/*global fetch */
+
 function search() {
     const keyword = document.getElementById("keyword").value;
     console.log(keyword);
     
     // get results from API using fetch
-    https://www.goodreads.com/search/index.xml?key=syJG0KciOSzBmzFcpRYw&q=Ender%27s+Game
-    const endpoint = "https://www.goodreads.com/search/index.xml"
-    const apiKey = ""
+    console.log(goodreadsApiKey);
+    const endpoint = "https://www.goodreads.com/search/index.xml?key=";
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const url = proxy + endpoint + "?key=" + goodreadsApiKey + "&q=" + keyword;
     
     fetch(url)
     .then(function(response) {
-        
-    })
+        console.log(response);
+    });
 }
